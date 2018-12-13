bcb3cb6e0115c4a8427fe5fb6136bd3bd93e21cb
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-13 10:05:09 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
   //creo var momentanea
-  // var numeroGiocaotori = 2
   //creao array giocatori
   var legaBasket = [];
-  ;
-
-  //var numeroGiocatori = 2;
-
   //eseguo la chiamata
   $.ajax({
 
-    url: "https://www.boolean.careers/api/array/basket?n=10",
+    url: "https://www.boolean.careers/api/array/basket?n=10" ,
     method: "GET",
+
     success: function(data, stato) {
       var info = data.response;
       for (var i = 0; i < info.length; i++) {
